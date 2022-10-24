56e543cf0a2d131d3de3687e3650006d023b1349
ale_maci <alessandromaci96@gmail.com>
2022-10-24 14:58:49 +0200

    removing unused function

--- contracts/interface/IDRS.sol
+++ contracts/interface/IDRS.sol
@@ -5,8 +5,6 @@ pragma solidity 0.8.17;
 interface IDRS {
     function register(address _address) external;
 
-    function increment() external;
-
     function getRegistered(address _address)
         external
         returns (bool _registered);
