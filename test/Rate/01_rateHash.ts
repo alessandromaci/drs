72d22b829bce0e44108eca8b2450fb3140109221
ale_maci <alessandromaci96@gmail.com>
2022-10-28 16:52:54 +0200

    updating tests

--- test/Rate/01_rateHash.ts
+++ test/Rate/01_rateHash.ts
@@ -20,15 +20,15 @@ describe("Rate Hash", async () => {
     txHash =
       "0x0d2c4fb86f67d31af9b467e8562c51f86a6ea0e20fdded0d175d48da1c5117d4";
     drs = await (await ethers.getContractFactory("DRS")).deploy();
-    contract = await (
-      await ethers.getContractFactory("RateHash")
-    ).deploy(drs.address);
+    contract = (await ethers.getContractFactory("RateHash")).attach(
+      drs.hashContract()
+    );
   });
 
   it("User should be registered and recorded in the DRS contract", async () => {
     const registerTx = contract.registerNew(address);
     await expect(registerTx).to.emit(drs, "NewRegistration").withArgs(address);
-    expect(await drs.registered(address)).to.equal(true);
+    expect(await drs.registered(address)).to.be.true;
   });
 
   it("User can rate and the rating is recorded", async () => {
@@ -37,8 +37,8 @@ describe("Rate Hash", async () => {
     await expect(rateTx)
       .to.emit(contract, "NewRating")
       .withArgs(sender, address, txHash, 50);
-    expect(await contract.hashRated(sender, txHash)).to.equal(true);
-    expect((await contract.rating(address)).score).to.equal(50);
+    expect(await drs.hashRated(sender, txHash)).to.be.true;
+    expect((await drs.hashRating(address)).score).to.equal(50);
   });
 
   it("User can't rate if the rated user is not registered", async () => {
